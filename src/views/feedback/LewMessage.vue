8c6acfaea5e8289c045fc628c640bdad0a56e078
lew <68851669+lewkamtao@users.noreply.github.com>
2022-06-21 17:43:59 +0800

    🐞 fix:add message code

--- src/views/feedback/LewMessage.vue
+++ src/views/feedback/LewMessage.vue
@@ -4,7 +4,34 @@ import { LewTitle } from '../../../packages';
 import DemoMessage1 from '../../demo/feedback/message/DemoMessage1.vue';
 import DemoBox from '../../layout/DemoBox.vue';
 
-let pre1 = ref(``);
+let pre1 = ref(`<script setup lang="ts">
+import { LewButton, LewMessage } from '../../../../packages';
+<\/script>
+
+<template> 
+    <lew-button type="danger" @click="LewMessage.error('这是一个demo信息')"
+        >error</lew-button
+    >
+    <lew-button type="success" @click="LewMessage.success('这是一个demo信息')"
+        >success</lew-button
+    >
+    <lew-button type="normal" @click="LewMessage.normal('这是一个demo信息')"
+        >normal</lew-button
+    >
+    <lew-button type="primary" @click="LewMessage.info('这是一个demo信息')"
+        >info</lew-button
+    >
+    <lew-button type="warning" @click="LewMessage.warning('这是一个demo信息')"
+        >warning</lew-button
+    >
+</template>
+
+<style lang="scss" scoped>
+.lew-button { 
+    margin-right: 20px;
+}
+</style>
+`);
 </script>
 
 <template>
