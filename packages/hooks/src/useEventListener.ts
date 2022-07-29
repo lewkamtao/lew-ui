65b096e0dfb72c64e8c5d6fa3f1c544141dfa1f1
sechi <sechichen@gmail.com>
2022-07-29 11:15:43 +0800

    fix: fix type

--- packages/hooks/src/useEventListener.ts
+++ packages/hooks/src/useEventListener.ts
@@ -1,6 +1,10 @@
 import { onMounted, onUnmounted } from 'vue';
 
-export function useEventListener(target: EventTarget, event: string, callback: () => void) {
+export function useEventListener(
+    target: EventTarget,
+    event: string,
+    callback: EventListenerOrEventListenerObject,
+): void {
     onMounted(() => target.addEventListener(event, callback, true));
     onUnmounted(() => target.removeEventListener(event, callback, true));
 }
