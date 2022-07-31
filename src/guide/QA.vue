940df89d31d45fabbbf34ec7505fe2d7631c0aa1
lew <68851669+lewkamtao@users.noreply.github.com>
2022-07-31 19:11:01 +0800

    docs: 更新文档

---
+++ src/guide/QA.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let pre1 = ref(`import { LewTooltip } from 'lew-ui';
+
+app.use(LewTooltip);`);
+let pre2 = ref(`import { LewDialog, LewMessage } from 'lew-ui';
+
+const open = () => {
+    LewDialog.error({
+        title: '删除确认',
+        content: '删除之后无法恢复，请确定',
+        ok: () => {
+            LewMessage.info('这是一个demo信息');
+        },
+        cancel: () => {
+            LewMessage.info('已取消');
+        },
+    });
+};`);
+</script>
+
+<template>
+    <div class="guide-wrapper">
+        <lew-title>Q&A</lew-title>
+        <br />
+        <br />
+        <lew-title size="16px">如何使用 Tooltips </lew-title>
+        <p>
+            tooltips 是一个自定义指令，你需要在
+            <lew-mark>main.ts</lew-mark> 注册该指令
+        </p>
+
+        <div v-highlight class="hl-pre">
+            <div class="pre-box">
+                <pre><code v-text="pre1"></code></pre>
+            </div>
+        </div>
+        <br />
+        <br />
+        <lew-title size="16px">关于 Dialog 和 Message 的用法</lew-title>
+        <p>如果你使用的方式不是全局引入，你需要引入它们才能使用</p>
+        <div v-highlight class="hl-pre">
+            <div class="pre-box">
+                <pre><code v-text="pre2"></code></pre>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/guide/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+    <lew-title>Todo List</lew-title>
+    <br />
+    <div>
+        <ul>
+            <li>Step 步骤</li>
+            <li>Upload 上传</li>
+            <li>Date Picker 日期选择器</li>
+            <li>Card 卡片</li>
+            <li>Collapse 评论</li>
+            <li>Progress 进度条</li>
+            <li>Loading 加载</li>
+        </ul>
+    </div>
+</template>
+<style lang="scss" scoped>
+ul {
+    padding-left: 20px;
+    line-height: 35px;
+}
+</style>
