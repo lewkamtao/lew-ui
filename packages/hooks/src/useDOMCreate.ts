008d2eca335dd9039c96f988c30bca130c80a6a8
lew <div@kamtao.com>
2023-05-12 18:38:47 +0800

    fix: 修复

--- packages/hooks/src/useDOMCreate.ts
+++ packages/hooks/src/useDOMCreate.ts
@@ -1,12 +1,10 @@
 export function useDOMCreate(nodeId: string): void {
-    let node = document.getElementById(nodeId);
-    if (node) {
-        document.body.appendChild(node);
-    } else {
-        node = document.createElement('div');
-        node.id = nodeId;
-    }
+    let node = document.getElementById(nodeId) || document.createElement('div');
+    node.id = nodeId;
+    document.body.appendChild(node);
     onUnmounted(() => {
-        if (node) document.body.removeChild(node);
+        try {
+            if (node) document.body.removeChild(node);
+        } catch {}
     });
 }
