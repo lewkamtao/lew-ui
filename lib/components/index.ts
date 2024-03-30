815f08b6f8553c2bf751c0b611166d308a8cbee5
lew <div@kamtao.com>
2024-03-30 13:21:02 +0800

    feat: new version

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -4,8 +4,8 @@ export * from './_layout';
 export * from './mark';
 export * from './icon';
 export * from './cascader';
-export * from './tree-select';
-export * from './tree';
+// export * from './tree-select';
+// export * from './tree';
 
 export * from './tabs';
 export * from './_steps';
--- src/layout/TheHeader.vue
+++ src/layout/TheHeader.vue
@@ -2,7 +2,7 @@
     import { useRoute, useRouter } from 'vue-router';
     import { useDark } from '@vueuse/core';
 
-    const version = ref('1.9.8');
+    const version = ref('2.1.3');
 
     const isDark = useDark({
         selector: 'html',
