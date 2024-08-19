1e7e438a53929cb3fafe1ec1f3300a12f623e383
lew <div@kamtao.com>
2024-08-19 20:48:16 +0800

    feat: v2.2.7

--- lib/docs/layout/TheHeader.vue
+++ lib/docs/layout/TheHeader.vue
@@ -2,7 +2,7 @@
 import { useRoute, useRouter } from 'vue-router'
 import { useDark } from '@vueuse/core'
 
-const version = ref('2.2.5')
+const version = ref('2.2.7')
 
 const isDark = useDark({
   selector: 'html',
