2599ae3b3c43dc568e30f561d5d5b17b91653ebb
lew <div@kamtao.com>
2024-11-18 15:56:17 +0800

    fix: icon

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,6 +1,9 @@
+/**
+ * 主题色类型
+ */
 export type LewThemeColor =
   | 'black'
-  | 'blue'
+  | 'blue' 
   | 'brown'
   | 'cyan'
   | 'gray'
@@ -14,6 +17,9 @@ export type LewThemeColor =
   | 'teal'
   | 'yellow'
 
+/**
+ * 状态色类型
+ */
 export type LewStatusColor =
   | 'error'
   | 'success'
@@ -22,9 +28,17 @@ export type LewStatusColor =
   | 'normal'
   | 'primary'
 
-// 合并主题色和状态色
+/**
+ * 颜色类型 = 主题色 + 状态色
+ */
 export type LewColor = LewThemeColor | LewStatusColor
 
+/**
+ * 尺寸类型
+ */
 export type LewSize = 'small' | 'medium' | 'large'
 
+/**
+ * 方向类型
+ */
 export type LewDirection = 'x' | 'y'
