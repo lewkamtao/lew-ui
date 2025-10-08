c081c4ff0a1d78c5873dbf0fd54ed4eb88ad1d57
lew <div@kamtao.com>
2025-10-08 21:06:12 +0800

    fix: form check

--- docs/docs/upload/demo/DemoUpload5.vue
+++ docs/docs/upload/demo/DemoUpload5.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import type { LewUploadFileItem } from 'lew-ui'
-import uploadHelper from '../uploadHelper'
+import type { LewUploadFileItem } from "lew-ui";
+import uploadHelper from "../uploadHelper";
 
-const fileList = ref<LewUploadFileItem[]>([])
+const fileList = ref<LewUploadFileItem[]>([]);
 </script>
 
 <template>
