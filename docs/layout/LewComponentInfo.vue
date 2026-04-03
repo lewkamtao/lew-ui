e8491102f597a4f7f9f6bf0a80b4dd7c876ee1a4
lew <div@kamtao.com>
2026-04-03 15:47:34 +0800

    remove: mdc

--- docs/layout/LewComponentInfo.vue
+++ docs/layout/LewComponentInfo.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { getMdcPath, renderDescription } from 'docs/lib/utils'
+import { renderDescription } from 'docs/lib/utils'
 import RenderComponent from 'lew-ui/_components/RenderComponent.vue'
-import LewMdcDrawer from '../components/LewMdcDrawer.vue'
 
 const route = useRoute()
 
@@ -14,47 +13,12 @@ function getComponentName() {
 }
 
 const componentName = computed(() => getComponentName())
-const mdcPath = computed(() => getMdcPath(componentName.value))
-const showMdcDrawer = ref(false)
-const mdcFileExists = ref(false)
-
-// 通过 import.meta.glob 让 Vite 在构建时静态分析并打包所有 mdc 文件
-const mdcModules = import.meta.glob('../../lib/**/*.mdc', { query: '?raw', import: 'default' })
-
-// 检查 mdc 文件是否存在（直接查映射表，无需异步）
-function checkMdcFileExists() {
-  mdcFileExists.value = !!mdcPath.value && (mdcPath.value in mdcModules)
-}
-
-// 监听路由变化，重新检查文件
-watch(
-  () => route.path,
-  () => {
-    checkMdcFileExists()
-  },
-  { immediate: true },
-)
-
-function openMdcDrawer() {
-  if (mdcPath.value && mdcFileExists.value) {
-    showMdcDrawer.value = true
-  }
-}
 </script>
 
 <template>
   <div>
     <div class="component-title-wrapper">
       <lew-title>{{ $t(`components.${componentName}.name`) }}</lew-title>
-      <lew-tag
-        v-if="mdcPath && mdcFileExists"
-        type="light"
-        color="blue"
-        style="margin-left: 8px; cursor: pointer"
-        @click="openMdcDrawer"
-      >
-        MDC
-      </lew-tag>
     </div>
     <div class="sub-title">
       <RenderComponent
@@ -63,11 +27,6 @@ function openMdcDrawer() {
         "
       />
     </div>
-    <LewMdcDrawer
-      v-if="mdcPath && mdcFileExists"
-      v-model:visible="showMdcDrawer"
-      :mdc-path="mdcPath"
-    />
   </div>
 </template>
 
